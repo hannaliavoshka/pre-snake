1d5b127810e2d67bff3aab4d86929485c1d4a18c
Hanna Liavoshka <hannaliavoshka@gmail.com>
2019-09-06 17:02:51 +0300

    Commit 7 A problem with adding cells in Group

--- src/GameField.java
+++ src/GameField.java
@@ -1,41 +1,37 @@
 
-public class GameField  {
+public class GameField {
 
     public static final int SIDE_CELLS_QUANTITY = 10;
+    public Cell[][] cells;
 
-    //!!!!Don't know how to implement it in this class
-    public Cell getCell(int x,int y){
-        return 
+
+    public Cell getCell(int x, int y) {
+        return cells[x][y];
     }
 
     // creates a Game Field
-    public GameField(){
-        Cell[][] cellField = initField();
+    public GameField() {
+        initField();
     }
 
     // adds cells in the Game Field
-    private Cell[][] initField(){
-        Cell[][] cells = new Cell[SIDE_CELLS_QUANTITY][SIDE_CELLS_QUANTITY];
+    private void initField() {
+        cells = new Cell[SIDE_CELLS_QUANTITY][SIDE_CELLS_QUANTITY];
         int x = 0;
         int y = 0;
-        for (Cell[] lineOfCells : cells){
-            for (Cell cell : lineOfCells){
-                cell = new Cell(x,y);
-                x+=Cell.CELL_SIZE;
+        for (Cell[] lineOfCells : cells) {
+            for (Cell cell : lineOfCells) {
+                cell = new Cell(x, y);
+                // проверка, удалить потом. Выводит данные клетки, то есть на данном этапе объект клетки существует.
+                System.out.println(cell);
+                x += Cell.CELL_SIZE;
             }
-            y+=Cell.CELL_SIZE;
-            x=0;
+            y += Cell.CELL_SIZE;
+            x = 0;
         }
-        return cells;
     }
 
-
-
-
-
-
-
-    }
+}
 
 
 
--- src/Window.java
+++ src/Window.java
@@ -8,18 +8,29 @@ public class Window extends Application {
     private double windowSize = GameField.SIDE_CELLS_QUANTITY * Cell.CELL_SIZE;
 
     //Will add all children to rootGroup
-    public void setStage(GameField gameField) {
+    public void fillGroup(GameField gameField, Group group) {
+
+        for (Cell[] cellLine : gameField.cells) {
+            for (Cell unit : cellLine) {
+                try {
+                    System.out.println("Unit " + unit); // проверка, удалить потом. На данном этапе каждый unit - null
+                    group.getChildren().addAll(unit);
+                } catch (NullPointerException e) {
+                    //e.printStackTrace();
+                    //break;
+                }
+            }
+        }
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         Group rootGroup = new Group();
         primaryStage.setTitle(WINDOW_TITLE);
         Scene rootScene = new Scene(rootGroup, windowSize, windowSize);
         primaryStage.setScene(rootScene);
         GameField gameField = new GameField();
-        setStage(gameField);
-
+        fillGroup(gameField, rootGroup);
         primaryStage.show();
     }
 
